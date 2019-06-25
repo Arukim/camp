1b804f4e16d97004b91ba5fb1cf29564322515d5
Nikita Smelov <Nikita_Smelov@epam.com>
2019-06-25 05:45:44 +0300

    .net impl of VEB tree started

---
+++ Trees/VEBTree/Tree.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VEBTree
+{
+
+    public class Tree
+    {
+        int min;
+        int max;
+        int M;
+        Tree[] children;
+        Tree aux;
+
+        public Tree(int m)
+        {
+            max = -1;
+            min = M = m;
+        }
+
+        public void Insert(int x)
+        {
+            if (min > max) // tree is empty
+            {
+                min = max = x;
+                return;
+            }
+
+            if (x < min)
+                (x, min) = (min, x);
+
+            if (x > max)
+                max = x;
+
+            var i = x / (int)Math.Sqrt(M);
+            var lo = i % (int)Math.Sqrt(M);
+            children[i].Insert(lo);
+
+            if (children[i].min == children[i].max)
+                aux.Insert(i);
+        }
+    }
+}
